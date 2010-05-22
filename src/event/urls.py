d6b919fc3c344e4569e8503d2e4a8ed63719f8d6
Henrique Bastos <henrique@bastos.net>
2010-05-22 12:21:54 -0300

    Urls to evento/estacio

--- src/event/urls.py
+++ src/event/urls.py
@@ -6,9 +6,9 @@ from lncc import talks as talks_for_lncc
 from estacio import talks as talks_for_estacio
 
 urlpatterns = patterns('django.views.generic.simple',
-    (r'istcc-p/$', 'direct_to_template', {'template': 'event.html', 'extra_context': {'talks': talks_for_lncc} }),
+    (r'istcc-p/$', 'direct_to_template', {'template': 'event/lncc.html', 'extra_context': {'talks': talks_for_lncc} }),
     (r'istcc-p/', include('subscription.urls')),
 
-    (r'estacio/$', 'direct_to_template', {'template': 'event.html', 'extra_context': {'talks': talks_for_estacio} }),
+    (r'estacio/$', 'direct_to_template', {'template': 'event/estacio.html', 'extra_context': {'talks': talks_for_estacio} }),
     #(r'^evento/estacio/', include('subscription.urls')),
 )
